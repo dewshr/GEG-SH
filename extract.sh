b28703f894db7210ab35fd684fd9a442865d2498
dewshr <dewshrs@gmail.com>
2021-09-20 23:48:37 -0500

    updated tar.gz extraction

--- extract.sh
+++ extract.sh
@@ -2,3 +2,4 @@
 
 tar xzvf ./data/blood_active_repressive_region.tar.gz -C ./data
 tar xzvf ./data/brain_active_repressive_region.tar.gz -C ./data
+tar xzvf ./data/blood_hic_interaction.bed.tar.gz -C ./data
